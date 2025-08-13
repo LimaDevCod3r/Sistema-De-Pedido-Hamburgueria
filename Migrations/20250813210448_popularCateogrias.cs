7f6ff338df1c13764529d49c8aa0397ee586a744
Diego Lima <diegolima20100@gmail.com>
2025-08-13 18:22:08 -0300

    feat: Populando as tabela category e Hamburgueres

---
+++ Migrations/20250813210448_popularCateogrias.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaDePedidoHamburgueria.Migrations
+{
+    /// <inheritdoc />
+    public partial class popularCateogrias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categorias (Nome, UrlImagem) VALUES('Hambúrgueres Tradicionais', 'https://exemplo.com/tradicionais.jpg')");
+            migrationBuilder.Sql("INSERT INTO Categorias (Nome, UrlImagem) VALUES('Hambúrgueres Gourmet', 'https://exemplo.com/gourmet.jpg')");
+            migrationBuilder.Sql("INSERT INTO Categorias (Nome, UrlImagem) VALUES('Hambúrgueres Vegetarianos', 'https://exemplo.com/vegetarianos.jpg')");
+            migrationBuilder.Sql("INSERT INTO Categorias (Nome, UrlImagem) VALUES('Hambúrgueres Vegetarianos', 'https://exemplo.com/vegetarianos.jpg')");
+            migrationBuilder.Sql("INSERT INTO Categorias (Nome, UrlImagem) VALUES('Bebidas', 'https://exemplo.com/bebidas.jpg')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Categorias");
+        }
+    }
+}
---
+++ Migrations/20250813211253_popularHamburgueres.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaDePedidoHamburgueria.Migrations
+{
+    /// <inheritdoc />
+    public partial class popularHamburgueres : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Hamburgueres(Nome, Descricao, Preco, UrlImagem, CategoriaId) VALUES('X-Bacon', 'Pão, hambúrguer, queijo, bacon crocante, alface e tomate', 25.90, 'https://exemplo.com/x-bacon.jpg', 1)");
+            migrationBuilder.Sql("INSERT INTO Hamburgueres(Nome, Descricao, Preco, UrlImagem, CategoriaId) VALUES('X-Salada', 'Pão, hambúrguer, queijo, alface, tomate, cebola e maionese', 22.50, 'https://exemplo.com/x-salada.jpg', 1)");
+            migrationBuilder.Sql("INSERT INTO Hamburgueres(Nome, Descricao, Preco, UrlImagem, CategoriaId) VALUES('X-Tudo', 'Pão, 2 hambúrgueres, queijo, bacon, ovo, alface, tomate, cebola e maionese', 32.90, 'https://exemplo.com/x-tudo.jpg', 1)");
+            migrationBuilder.Sql("INSERT INTO Hamburgueres(Nome, Descricao, Preco, UrlImagem, CategoriaId) VALUES('Gourmet Trufado', 'Pão brioche, hambúrguer artesanal, queijo trufado, cogumelos e molho especial', 45.00, 'https://exemplo.com/gourmet-trufado.jpg', 2)");
+            migrationBuilder.Sql("INSERT INTO Hamburgueres(Nome, Descricao, Preco, UrlImagem, CategoriaId) VALUES('Veggie Burger', 'Hambúrguer de grão-de-bico, queijo vegano, alface, tomate e molho especial', 28.90, 'https://exemplo.com/veggie.jpg', 3)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Hamburgueres");
+        }
+    }
+}
